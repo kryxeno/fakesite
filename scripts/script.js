c3e11017ff275c924ffc57ad2c58aedb0e0ac50c
Mr Bats <timvaningen63@gmail.com>
2022-11-17 17:07:14 +0100

    Burger menu is working now

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,20 @@
 // JavaScript Document
 console.log("hi");
+
+const burgerIcon = document.querySelector("nav div div a:first-child");
+const closeIcon = document.querySelector("nav aside ul li:first-child a");
+const menu = document.querySelector("nav aside");
+let open = true;
+
+function burgerMenu() {
+  if (open) {
+    menu.classList.remove("menuOpen");
+    open = false;
+  } else {
+    menu.classList.add("menuOpen");
+    open = true;
+  }
+}
+
+burgerIcon.addEventListener("click", burgerMenu);
+closeIcon.addEventListener("click", burgerMenu);
