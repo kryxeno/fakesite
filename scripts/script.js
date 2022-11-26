74fb3ecb5b153fc7cfdf4e795441071e69862d40
Mr Bats <timvaningen63@gmail.com>
2022-11-26 03:26:13 +0100

    added dark mode switch and no scroll

--- scripts/script.js
+++ scripts/script.js
@@ -11,12 +11,14 @@ let open = false;
 function burgerMenu() {
   if (open) {
     sideMenu.classList.remove("menuOpen");
+    document.documentElement.classList.remove("no-scroll");
     open = false;
     burgerIcon.setAttribute("aria-expanded", open);
     closeIcon.setAttribute("aria-expanded", open);
     console.log("Sidebar extended = " + open);
   } else {
     sideMenu.classList.add("menuOpen");
+    document.documentElement.classList.add("no-scroll");
     open = true;
     burgerIcon.setAttribute("aria-expanded", open);
     closeIcon.setAttribute("aria-expanded", open);
@@ -36,4 +38,45 @@ adverts.forEach((advert) => {
   button.addEventListener("click", () => {
     advert.classList.add("removeAd");
   });
+});
+
+// https://www.ditdot.hr/en/dark-mode-website-tutorial
+let darkModeState = false;
+
+const darkButton = document.querySelector("header nav section:last-of-type ul:last-of-type button");
+const modeTxt = document.querySelector("header nav section:last-of-type ul:last-of-type button span:nth-child(2)");
+
+// MediaQueryList object
+const useDark = window.matchMedia("(prefers-color-scheme: dark)");
+
+// Toggles the "dark-mode" class
+function toggleDarkMode(state) {
+  document.documentElement.classList.toggle("dark-mode", state);
+  darkModeState = state;
+  if (darkModeState) {
+    modeTxt.textContent = "Light UI";
+  } else {
+    modeTxt.textContent = "Dark UI";
+  }
+}
+
+// Sets localStorage state
+function setDarkModeLocalStorage(state) {
+  localStorage.setItem("dark-mode", state);
+}
+
+// Initial setting
+toggleDarkMode(localStorage.getItem("dark-mode") == "true");
+
+// Listen for changes in the OS settings.
+// Note: the arrow function shorthand works only in modern browsers,
+// for older browsers define the function using the function keyword.
+useDark.addListener((evt) => toggleDarkMode(evt.matches));
+
+// Toggles the "dark-mode" class on click and sets localStorage state
+darkButton.addEventListener("click", () => {
+  darkModeState = !darkModeState;
+
+  toggleDarkMode(darkModeState);
+  setDarkModeLocalStorage(darkModeState);
 });
