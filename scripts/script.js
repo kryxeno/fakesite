1f5edfa6da26a49cdcd29904f5118e7fc4ab619b
Mr Bats <timvaningen63@gmail.com>
2022-12-04 01:34:10 +0100

    added movehints with friendly and foe detection

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,4 @@
 // JavaScript Document
-console.log("hi");
 
 // sidebar menu
 
@@ -105,6 +104,8 @@ chessPieces.forEach((piece) => {
   let ogPosY = 0;
   let currentDroppable = null;
   let elemBelow = null;
+  let pieceColor = null;
+  let pieceType = null;
 
   piece.onmousedown = function (event) {
     // getting board bounds
@@ -122,6 +123,278 @@ chessPieces.forEach((piece) => {
     // show orange square under selected piece
     selectedPiece.classList.remove("visually-hidden");
     selectedPiece2.classList.add("visually-hidden");
+
+    // removes any previous hints
+    let hints = document.querySelectorAll(".moves");
+    hints.forEach((hint) => {
+      hint.remove();
+    });
+
+    // drawing possible moves
+    drawMoves();
+
+    // https://codepen.io/DevilSAM/pen/ZNoByE
+    // wont actually be used
+    // it is just for visual representation
+    const chess = [
+      [18, 28, 38, 48, 58, 68, 78, 88],
+      //
+      [17, 27, 37, 47, 57, 67, 77, 87],
+      //
+      [16, 26, 36, 46, 56, 66, 76, 86],
+      //
+      [15, 25, 35, 45, 55, 65, 75, 85],
+      //
+      [14, 24, 34, 44, 54, 64, 74, 84],
+      //
+      [13, 23, 33, 43, 53, 63, 73, 83],
+      //
+      [12, 22, 32, 42, 52, 62, 72, 82],
+      //
+      [11, 21, 31, 41, 51, 61, 71, 81],
+    ];
+
+    function drawMoves() {
+      // resets piece details
+      pieceColor = null;
+      pieceType = null;
+
+      // determines black or white piece (pieceColor)
+      for (let i = piece.classList.length - 1; i >= 0; i--) {
+        const className = piece.classList[i];
+        if (className.startsWith("b")) {
+          pieceColor = true;
+        } else if (className.startsWith("w")) {
+          pieceColor = false;
+        }
+      }
+
+      // determines black or white piece (pieceColor)
+      for (let i = piece.classList.length - 1; i >= 0; i--) {
+        const className = piece.classList[i];
+        if (className.endsWith("r")) {
+          pieceType = "rook";
+        } else if (className.endsWith("n")) {
+          pieceType = "knight";
+        } else if (className.endsWith("b")) {
+          pieceType = "bishop";
+        } else if (className.endsWith("k")) {
+          pieceType = "king";
+        } else if (className.endsWith("q")) {
+          pieceType = "queen";
+        } else if (className.endsWith("p")) {
+          pieceType = "pawn";
+        }
+      }
+
+      // log piece details
+      if (pieceColor) {
+        console.log("black" + " " + pieceType);
+      } else {
+        console.log("white" + " " + pieceType);
+      }
+
+      // calculates moves
+      if (pieceType == "rook") {
+        let X = roundDownToNearest1(ogPosX);
+        let Y = roundDownToNearest1(ogPosY);
+        for (let i = 1; i < 9; i++) {
+          if (i == Y) {
+            for (let j = 1; j < 9; j++) {
+              if (j == X) continue;
+              const hint = document.createElement("div");
+              board.appendChild(hint);
+              hint.classList.add("box", "moves", "square-" + j + "" + Y);
+            }
+          }
+          if (i == Y) continue;
+          const hint = document.createElement("div");
+          board.appendChild(hint);
+          hint.classList.add("box", "moves", "square-" + X + "" + i);
+        }
+      } else if (pieceType == "knight") {
+        let X = roundDownToNearest1(ogPosX);
+        let Y = roundDownToNearest1(ogPosY);
+        let j = 2;
+        for (let i = 1; i < 3; i++) {
+          if (0 < X - i && X - i < 9 && 0 < Y + j && Y + j < 9) {
+            const hint = document.createElement("div");
+            board.appendChild(hint);
+            hint.classList.add("box", "moves", "square-" + (X - i) + "" + (Y + j));
+          }
+          if (0 < X - i && X - i < 9 && 0 < Y - j && Y - j < 9) {
+            const hint = document.createElement("div");
+            board.appendChild(hint);
+            hint.classList.add("box", "moves", "square-" + (X - i) + "" + (Y - j));
+          }
+          if (0 < X + i && X + i < 9 && 0 < Y + j && Y + j < 9) {
+            const hint = document.createElement("div");
+            board.appendChild(hint);
+            hint.classList.add("box", "moves", "square-" + (X + i) + "" + (Y + j));
+          }
+          if (0 < X + i && X + i < 9 && 0 < Y - j && Y - j < 9) {
+            const hint = document.createElement("div");
+            board.appendChild(hint);
+            hint.classList.add("box", "moves", "square-" + (X + i) + "" + (Y - j));
+          }
+          j--;
+        }
+      } else if (pieceType == "bishop") {
+        let X = roundDownToNearest1(ogPosX);
+        let Y = roundDownToNearest1(ogPosY);
+        for (let i = 1; i < 9; i++) {
+          for (let j = 1; j < 9; j++) {
+            if (i == X && j == Y) continue;
+            if (j - i == Y - X) {
+              if (0 < i && i < 9 && 0 < j && j < 9) {
+                const hint = document.createElement("div");
+                board.appendChild(hint);
+                hint.classList.add("box", "moves", "square-" + i + "" + j);
+              }
+            }
+          }
+        }
+        for (let i = 1; i < 9; i++) {
+          for (let j = 1; j < 9; j++) {
+            if (i == X && j == Y) continue;
+            if (j + i == Y + X) {
+              if (0 < i && i < 9 && 0 < j && j < 9) {
+                const hint = document.createElement("div");
+                board.appendChild(hint);
+                hint.classList.add("box", "moves", "square-" + i + "" + j);
+              }
+            }
+          }
+        }
+      } else if (pieceType == "king") {
+        let X = roundDownToNearest1(ogPosX);
+        let Y = roundDownToNearest1(ogPosY);
+        for (let i = X - 1; i < X + 2; i++) {
+          for (let j = Y - 1; j < Y + 2; j++) {
+            if (i == X && j == Y) continue;
+            if (0 < i && i < 9 && 0 < j && j < 9) {
+              const hint = document.createElement("div");
+              board.appendChild(hint);
+              hint.classList.add("box", "moves", "square-" + i + "" + j);
+            }
+          }
+        }
+      } else if (pieceType == "queen") {
+        let X = roundDownToNearest1(ogPosX);
+        let Y = roundDownToNearest1(ogPosY);
+        for (let i = 1; i < 9; i++) {
+          for (let j = 1; j < 9; j++) {
+            if (i == X && j == Y) continue;
+            if (j - i == Y - X) {
+              if (0 < i && i < 9 && 0 < j && j < 9) {
+                const hint = document.createElement("div");
+                board.appendChild(hint);
+                hint.classList.add("box", "moves", "square-" + i + "" + j);
+              }
+            }
+          }
+        }
+        for (let i = 1; i < 9; i++) {
+          for (let j = 1; j < 9; j++) {
+            if (i == X && j == Y) continue;
+            if (j + i == Y + X) {
+              if (0 < i && i < 9 && 0 < j && j < 9) {
+                const hint = document.createElement("div");
+                board.appendChild(hint);
+                hint.classList.add("box", "moves", "square-" + i + "" + j);
+              }
+            }
+          }
+        }
+        for (let i = 1; i < 9; i++) {
+          if (i == Y) {
+            for (let j = 1; j < 9; j++) {
+              if (j == X) continue;
+              const hint = document.createElement("div");
+              board.appendChild(hint);
+              hint.classList.add("box", "moves", "square-" + j + "" + Y);
+            }
+          }
+          if (i == Y) continue;
+          const hint = document.createElement("div");
+          board.appendChild(hint);
+          hint.classList.add("box", "moves", "square-" + X + "" + i);
+        }
+      } else if (pieceType == "pawn") {
+        let X = roundDownToNearest1(ogPosX);
+        let Y = roundDownToNearest1(ogPosY);
+        for (let i = 1; i < 3; i++) {
+          const hint = document.createElement("div");
+          board.appendChild(hint);
+          if (pieceColor && 0 < Y - i) {
+            hint.classList.add("box", "moves", "square-" + X + "" + (Y - i));
+          } else if (!pieceColor && Y + i < 9) {
+            hint.classList.add("box", "moves", "square-" + X + "" + (Y + i));
+          }
+        }
+      }
+    }
+    //check for same team
+    hints = document.querySelectorAll(".moves");
+    hints.forEach((hint) => {
+      let belowPieceColor = null;
+      const hintBounds = hint.getBoundingClientRect();
+      let hintPosX = hintBounds.right - (hintBounds.right - hintBounds.left) / 2;
+      let hintPosY = hintBounds.bottom - (hintBounds.bottom - hintBounds.top) / 2;
+      hints.forEach((hint) => {
+        hint.hidden = true;
+      });
+      let droppableBelow = document.elementFromPoint(hintPosX, hintPosY);
+      hints.forEach((hint) => {
+        hint.hidden = false;
+      });
+
+      // if (droppableBelow.classList.contains("piece")) {
+      //   console.log(droppableBelow);
+      // }
+
+      if (droppableBelow != null) {
+        if (droppableBelow.classList.contains("piece")) {
+          for (let i = droppableBelow.classList.length - 1; i >= 0; i--) {
+            const className = droppableBelow.classList[i];
+            if (className.startsWith("b")) {
+              belowPieceColor = true;
+            } else if (className.startsWith("w")) {
+              belowPieceColor = false;
+            }
+          }
+        }
+        if (belowPieceColor == pieceColor) {
+          hint.remove();
+        } else if (belowPieceColor == !pieceColor) {
+          hint.classList.add("targeted");
+        }
+      }
+    });
+
+    // take current location of piece (ogPos)
+
+    // take the color of the piece (if class starts with "b"= true / "w"= false) and type of piece (if class ends with "r"/"n"/"b"/"k"/"q"/"p")
+
+    // calculate and spit out all possible moves from that pos depending on piece:
+    // (using for-loops probably)
+
+    // example: bishop(53) gets +-11 and +-9
+    // possible move squares = 31 42 64 75 86 (+-11) and 17 26 35 44 62 71 (+-9)
+
+    // in a for loop draw elements for each number and add the square-"" class
+    // relative to the number
+
+    //    if a an enemy piece
+    //    (if class starts with "b"= true / "w"= false)
+    //    (the inverse of own color) is on the square (either log the positions of all the pieces)
+    //    draw a targeted square
+
+    //    if a friendly piece is on the square dont draw
+
+    //    a piece (except for knight) can only move up to the 1st piece in their path
+    //    so check if the piece encounters another piece and remove all hints after that point
+    //    if its an enemy draw a targeted hint instead
 
     // show hover square
     hover.classList.remove("visually-hidden");
@@ -165,10 +438,17 @@ chessPieces.forEach((piece) => {
       piece.hidden = true;
       hover.hidden = true;
       selectedPiece.hidden = true;
+      const hints = document.querySelectorAll(".moves");
+      hints.forEach((hint) => {
+        hint.hidden = true;
+      });
       elemBelow = document.elementFromPoint(event.clientX, event.clientY);
       piece.hidden = false;
       hover.hidden = false;
       selectedPiece.hidden = false;
+      hints.forEach((hint) => {
+        hint.hidden = false;
+      });
 
       // if there is no element below just stop the function here
       if (!elemBelow) return;
@@ -180,13 +460,11 @@ chessPieces.forEach((piece) => {
       if (currentDroppable != droppableBelow) {
         if (currentDroppable) {
           // null when we were not over a droppable before this event
-          currentDroppable.classList.remove("targeted");
         }
         currentDroppable = droppableBelow;
         if (currentDroppable) {
           // null if we're not coming over a droppable now
           // (maybe just left the droppable)
-          currentDroppable.classList.add("targeted");
         }
       }
     }
@@ -208,7 +486,7 @@ chessPieces.forEach((piece) => {
       // return piece to standard zIndex and remove transform
       piece.style.zIndex = null;
       piece.style.transform = null;
-      console.log("New Position =  " + roundDownToNearest1(lastPosX) + "" + roundDownToNearest1(lastPosY));
+      console.log("New Position = " + roundDownToNearest1(lastPosX) + "" + roundDownToNearest1(lastPosY));
 
       // check if the piece has moved to a different square
       if (roundDownToNearest1(lastPosX) == roundDownToNearest1(ogPosX) && roundDownToNearest1(lastPosY) == roundDownToNearest1(ogPosY)) {
@@ -223,7 +501,13 @@ chessPieces.forEach((piece) => {
           }
         }
         selectedPiece2.classList.add("square-" + roundDownToNearest1(lastPosX) + "" + roundDownToNearest1(lastPosY));
+        const hints = document.querySelectorAll(".moves");
+        hints.forEach((hint) => {
+          hint.remove();
+        });
       }
+
+      //remove all hints when finishing turn
 
       // checks what the targeted square is and what should happen to the pieces involved
       if ("enemy") {
